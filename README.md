0fa1d1f796c7d09104d93d7bda3c9d48b1e3442d
tungyao <1922527051@qq.com>
2020-03-11 17:54:34 +0800

    update a little

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-"# cedar-session" 
+# session component for cedar
+## usage
+* step1 `r:=cedar.NewRouter()`
+* step2 `x := cedar_session.NewSession(r)`
+* step3
+```go
+// you can use all router func
+x.Get("/",http.HandlerFunc,http.Handler)
+// and session func
+x.SetSession()
+x.GetSession()
+```
+
+## one more thing
+* x.CreateUUID([]byte) ,transfer ip or other parameter to the first
+### get byte => `********-****************-****-****`
+### get byte =>  8-16-4-4
--- session.go
+++ session.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/sha1"
 	"fmt"
 	"github.com/tungyao/cedar"
 	"github.com/tungyao/spruce"
+	"log"
+	"math/rand"
 	"net/http"
 	"sync"
 	"time"
@@ -12,46 +15,103 @@ import (
 func main() {
 	r := cedar.NewRouter()
 	x := NewSession(r)
-	x.Get("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write(x.SetSession(nil, ""))
+	x.Get("/", func(writer http.ResponseWriter, request *http.Request, session *Session) {
+		session.SetSession()
 	}, nil)
 	http.ListenAndServe(":80", x)
+
 }
 func NewSession(hp *cedar.Trie) *Session {
+	log.Println("Session : starting")
 	h := spruce.CreateHash(1024)
 	s := &Session{
 		RWMutex: &sync.RWMutex{},
 		Mutex:   &sync.Mutex{},
-		Trie:    hp,
+		tree:    hp,
 		hash:    h,
+		Self:    newId(),
 	}
 	return s
 }
 
+type SessionSelf struct {
+	Id   string
+	Time int64
+}
 type Session struct {
 	*sync.RWMutex
-	*cedar.Trie
+	tree *cedar.Trie
 	hash *spruce.Hash
 	*sync.Mutex
+	Self []byte
 }
 
+func newId() []byte {
+	d := "abcdef012345689"
+	da := make([]byte, 4)
+	for i := 0; i < 4; i++ {
+		<-time.After(time.Nanosecond * 10)
+		da[i] = d[rand.New(rand.NewSource(time.Now().UnixNano())).Intn(15)]
+	}
+	log.Printf("Session : Create Session Id => %s", da)
+	return da
+}
 func (si *Session) GetSession(uuid string) {
 
+}
+func (si *Session) Get(path string, fn func(w http.ResponseWriter, r *http.Request, s *Session), hal http.Handler) {
+	//x := Sha1(si.CreateUUID())
+	//c1 := http.Cookie{
+	//	Name:     "session",
+	//	Value:    string(x),
+	//	HttpOnly: true,
+	//}
+	//w.Header().Set("Set-Cookie", c1.String())
+	//w.Header().Add("Set-Cookie", c1.String())
+	si.tree.Get(path, func(writer http.ResponseWriter, request *http.Request) {
+
+	}, hal)
 }
 
 //  you don't need to input a value of token when put a session key in first time
 // it's second times , you must input it
-func (si *Session) SetSession(token []byte, stt interface{}) []byte {
-	if token == nil {
-		<-time.After(1 * time.Nanosecond)
-		return si.CreateUUID(token, time.Now().UnixNano())
+// output sha-1
+func (si *Session) SetSession(w http.ResponseWriter, key []byte, body interface{}) []byte {
+	if key == nil {
+
+		return x
 	}
 	return nil
+}
+func Sha1(b []byte) []byte {
+	h := sha1.New()
+	h.Write(b)
+	return []byte(fmt.Sprintf("%x", h.Sum(nil)))
 }
 
 // UUID 64 bit
 // 8-4-4-12 16hex string
-func (si *Session) CreateUUID(name []byte, tms int64) []byte {
-	fmt.Printf("%x\n", tms)
-	return []byte(fmt.Sprintf("%x\n", tms))
+func (si *Session) CreateUUID(xtr []byte) []byte {
+	str := fmt.Sprintf("%x", xtr)
+	strLow := ComplementHex(str[:(len(str)-1)/3], 8)
+	strMid := ComplementHex(str[(len(str)-1)/3:(len(str)-1)*2/3], 4)
+	si.Mutex.Lock()
+	defer si.Mutex.Unlock()
+	<-time.After(1 * time.Nanosecond)
+	ti := time.Now().UnixNano()
+	return []byte(fmt.Sprintf("%s-%x-%s-%s", strLow, ti, strMid, si.Self))
+}
+func ComplementHex(s string, x int) string {
+	if len(s) == x {
+		return s
+	}
+	if len(s) < x {
+		for i := 0; i < x-len(s); i++ {
+			s += "0"
+		}
+	}
+	if len(s) > x {
+		return s[:x]
+	}
+	return s
 }
